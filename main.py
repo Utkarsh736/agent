0e24f614dfc17c1e3dd347abba2596fc1018a5e5
Utkarsh736 <utkarshtomar736@gmail.com>
2025-12-25 17:26:13 +0530

    [Init] Initital uv setup

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY not found. Add it to your .env file and restart your terminal.")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+        model='gemini-2.5-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.')
+
+print(response.text)
+
